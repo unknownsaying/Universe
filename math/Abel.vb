57cee1fc14517994c990642f63a2fbaadf4f8df7
unknownsaying <15368104568@189.cn>
2025-07-05 20:34:06 +0800

    Update Abel.vb

--- math/Abel.vb
+++ math/Abel.vb
@@ -1,4 +1,4 @@
-Imports Math
+Imports System.Math
 Imports "UPPERBIT.c"
 Imports "linear.py"
 Sub Abel
@@ -12,7 +12,7 @@ return  cosh(uv + t6) + sinh(www - t6)
 return  ceil(uv + t7) + floor(www - t7)
 return  floor(uv + t8) + ceil(www - t8)
 return  fmod(uv + t9) + modf(www - t9)
-return  fmod(uv + t10) + modf(www - t10)
+return  modf(uv + t10) + fmod(www - t10)
 return  exp(uv + t11) + log(www - t11)
 return  log(uv + t12) + exp(www - t12)
 End Sub
