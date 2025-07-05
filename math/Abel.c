9ee5f47a832ab02ba568e3f60afd778c3315af3f
unknownsaying <15368104568@189.cn>
2025-07-05 20:36:10 +0800

    Update and rename Abel.vb to Abel.c

--- math/Abel.c
+++ math/Abel.c
@@ -1,7 +1,6 @@
-Imports System.Math
-Imports "UPPERBIT.c"
-Imports "linear.py"
-Sub Abel
+#include "UPPERBIT.c"
+#include "linear.py"
+void Abel(int argc,char *argv[]){
 return  sin(uv + t1) + cos(www - t1)
 return  cos(uv + t2) + sin(www - t2)
 return  asin(uv + t3) + acos(www - t3)
@@ -15,4 +14,4 @@ return  fmod(uv + t9) + modf(www - t9)
 return  modf(uv + t10) + fmod(www - t10)
 return  exp(uv + t11) + log(www - t11)
 return  log(uv + t12) + exp(www - t12)
-End Sub
+}
