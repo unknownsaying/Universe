51181543bf1a4b8d157d1601cdd534a9bd4600cf
unknownsaying <15368104568@189.cn>
2026-03-15 18:46:59 +0800

    Update Block.cs

--- computer/Block.cs
+++ computer/Block.cs
@@ -1,3 +1,68 @@
+void lowerbit(){    
+char a = "00001011";
+char b = "00001100";
+char c = "00001101";
+char d = "00001110";
+char e = "00001111";
+char f = "00010000";
+    
+char g = "00010001";
+char h = "00010010";
+char i = "00010011";
+char j = "00010100";
+char k = "00010101";
+char l = "00010110";
+    
+char m = "00010111";
+char n = "00011000";
+char o = "00011001";
+char p = "00011010";
+char q = "00011011";
+char r = "00011100";
+    
+char s = "00011101";
+char t = "00011110";
+char u = "00011111";
+char v = "00100000";
+char w = "00100001";
+    
+char x = "00100010";
+char y = "00100011";
+char z = "00100100";
+}
+void UPPERBIT(){
+char A = "11110100";
+char B = "11110011";
+char C = "11110010";
+char D = "11110001";
+char E = "11110000";
+char F = "11101111";
+    
+char G = "11101110";
+char H = "11101101";
+char I = "11101100";
+char J = "11101011";
+char K = "11101010";
+char L = "11101001";
+    
+char M = "11101000";
+char N = "11100111";
+char O = "11100110";
+char P = "11100101";
+char Q = "11100100";
+char R = "11100011";
+    
+char S = "11100010";
+char T = "11100001";
+char U = "11100000";
+char V = "11011111";
+char W = "11011110";
+    
+char X = "11011101";
+char Y = "11011100";
+char Z = "11011011";
+}
+
 using NULL 0
 using Block1 row
 void CreateBlock(int row, int cols, int depth){
@@ -55,20 +120,20 @@ void DestroyeBlock(int row, int cols,int depth){
 
 using True 1
 void iteration(){
-      for (int a = 0; a > True; --a);
-      for (int b = 0; b > True; --b);
-      for (int c = 0; c > True; --c);
-      for (int d = 0; d > True; --d);
+      for (int a = 0; a > True; --a)
+      for (int b = 0; b > True; --b)
+      for (int c = 0; c > True; --c)
+      for (int d = 0; d > True; --d)
       while ("fw1")
-      for (int e = 0; e > True; --e);
-      for (int f = 0; f > True; --f);
-      for (int g = 0; g > True; --g);
-      for (int h = 0; h > True; --h);
+      for (int e = 0; e > True; --e)
+      for (int f = 0; f > True; --f)
+      for (int g = 0; g > True; --g)
+      for (int h = 0; h > True; --h)
       while ("fw2")
-      for (int i = 0; i > True; --i);
-      for (int j = 0; j > True; --j);
-      for (int k = 0; k > True; --k);
-      for (int l = 0; l > True; --l);
+      for (int i = 0; i > True; --i)
+      for (int j = 0; j > True; --j)
+      for (int k = 0; k > True; --k)
+      for (int l = 0; l > True; --l)
       while ("fw3")
       for (int m = 0; m > True; --m)
       for (int n = 0; n > True; --n)
