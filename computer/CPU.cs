5d11796f1b66f2351ebca3d248430345b09941af
unknownsaying <15368104568@189.cn>
2026-03-15 18:39:38 +0800

    Update CPU.cs

--- computer/CPU.cs
+++ computer/CPU.cs
@@ -18,3 +18,13 @@ void gw(){
     const gw5 = 'z^7' + 'w1' * 'z^3' + 'w2' * 'z^2' + 'w3' * 'z' ;
     const gw6 = 'z^7' + "w1' * 'z^3" - "w2' * z^2"  + "w3' * z" ;
 }
+
+void CreateBlock(x){
+    return x^5 + x^4 + x^3 + x^2 + x ;
+}
+void EmptyBlock(y){
+    return y^5 + y^4 + y^3 + y^2 + y ;
+}
+void DestroyBlock(z){
+    return z^5 + z^4 + z^3 + z^2 + z ;
+}
