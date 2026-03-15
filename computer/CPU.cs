abdc7e392939db1d1ca1dd6b22a4ad36dde2ca2b
unknownsaying <15368104568@189.cn>
2026-03-15 18:39:11 +0800

    Update CPU.cs

--- computer/CPU.cs
+++ computer/CPU.cs
@@ -10,3 +10,11 @@ void unit(){
       const  fw6 = "w1'z1 + w2'z2 + w3'z3";
 }
 
+void gw(){
+    const gw1 = 'x^7' + 'w1' * 'x^3' + 'w2' * 'x^2' + 'w3' * 'x' ;
+    const gw2 = 'x^7' + "w1' *  x^3" - "w2' * x^2"  + "w3' * x" ;
+    const gw3 = 'y^7' + 'w1' * 'y^3' + 'w2' * 'y^2' + 'w3' * 'y' ;
+    const gw4 = 'y^7' + "w1' * 'y^3" - "w2' * y^2"  + "w3' * y" ;
+    const gw5 = 'z^7' + 'w1' * 'z^3' + 'w2' * 'z^2' + 'w3' * 'z' ;
+    const gw6 = 'z^7' + "w1' * 'z^3" - "w2' * z^2"  + "w3' * z" ;
+}
